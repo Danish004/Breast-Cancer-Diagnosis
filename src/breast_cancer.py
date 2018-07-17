ad302f3df08128d1723a50375b6c486d6aabd20f
Sharim Pervez <sarimpervez@gmail.com>
2018-07-17 13:11:19 +0530

    Update breast_cancer.py

--- src/breast_cancer.py
+++ src/breast_cancer.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import neighbors, svm
 from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
+
+
+cancer = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data", header = None)
+cancer.to_csv("../data/bc.csv", encoding='utf-8', index=False, header=False)
